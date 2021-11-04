8110fd10aff6f2fa12bd36bed6f85ffa0b43572c
Kuratar <ednkage@gmail.com>
2021-11-03 22:36:52 -0700

    refactor: convert ms to time

--- client/src/app/components/track-list/track-list.component.ts
+++ client/src/app/components/track-list/track-list.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { TrackData } from '../../data/track-data';
+import { Component, OnInit, Input } from "@angular/core";
+import { TrackData } from "../../data/track-data";
+import convertTime from "../../../utils/convertTime";
 
 @Component({
-  selector: 'app-track-list',
-  templateUrl: './track-list.component.html',
-  styleUrls: ['./track-list.component.css']
+  selector: "app-track-list",
+  templateUrl: "./track-list.component.html",
+  styleUrls: ["./track-list.component.css"],
 })
 export class TrackListComponent implements OnInit {
-	@Input() tracks:TrackData[];
-	@Input() hideArtist:boolean = false;
-	@Input() hideAlbum:boolean = false;
+  @Input() tracks: TrackData[];
+  @Input() hideArtist: boolean = false;
+  @Input() hideAlbum: boolean = false;
+  time: string;
+  convertFunction: Function = convertTime;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- client/src/app/pages/track-page/track-page.component.ts
+++ client/src/app/pages/track-page/track-page.component.ts
@@ -1,38 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ArtistData } from '../../data/artist-data';
-import { TrackData } from '../../data/track-data';
-import { AlbumData } from '../../data/album-data';
-import { TrackFeature } from '../../data/track-feature';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ArtistData } from "../../data/artist-data";
+import { TrackData } from "../../data/track-data";
+import { AlbumData } from "../../data/album-data";
+import { TrackFeature } from "../../data/track-feature";
 import { SpotifyService } from "../../services/spotify.service";
+import convertTime from "../../../utils/convertTime";
 
 @Component({
-  selector: 'app-track-page',
-  templateUrl: './track-page.component.html',
-  styleUrls: ['./track-page.component.css']
+  selector: "app-track-page",
+  templateUrl: "./track-page.component.html",
+  styleUrls: ["./track-page.component.css"],
 })
 export class TrackPageComponent implements OnInit {
-	trackId:string;
-	track:TrackData;
-  audioFeatures:TrackFeature[];
+  trackId: string;
+  track: TrackData;
+  audioFeatures: TrackFeature[];
+  time: string;
 
   constructor(
     private route: ActivatedRoute,
     private spotifyService: SpotifyService
-  ) { }
+  ) {}
 
   ngOnInit() {
     // http://localhost:4200/track/4eYaDRhiL5iesFp2EuoODr
-  	this.trackId = this.route.snapshot.paramMap.get('id');
-  	//TODO: Inject the spotifyService and use it to get the track data and it's audio features
-    this.spotifyService.getTrack(this.trackId)
-    .then((trackData) => {
+    this.trackId = this.route.snapshot.paramMap.get("id");
+    //TODO: Inject the spotifyService and use it to get the track data and it's audio features
+    this.spotifyService.getTrack(this.trackId).then((trackData) => {
       this.track = trackData;
+      this.time = convertTime(this.track.duration_ms);
     });
-    this.spotifyService.getAudioFeaturesForTrack(this.trackId)
-    .then((featuresData) => {
-      this.audioFeatures = featuresData;
-    });
+    this.spotifyService
+      .getAudioFeaturesForTrack(this.trackId)
+      .then((featuresData) => {
+        this.audioFeatures = featuresData;
+      });
   }
 
+  // convertTime(ms: number) {
+  //   const minutes = Math.floor(ms / 60000);
+  //   const seconds = ((ms % 60000) / 1000).toFixed(0);
+  //   return minutes + ":" + (Number(seconds) < 10 ? "0" : "") + seconds;
+  // }
 }
