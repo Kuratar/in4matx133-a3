ad8d3fa4b91aefc6e52cf5b932551079e80cf03a
Andrew Ton <aton4@uci.edu>
2021-11-03 17:59:21 -0700

    feat: create album page

--- client/src/app/pages/album-page/album-page.component.ts
+++ client/src/app/pages/album-page/album-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ArtistData } from '../../data/artist-data';
 import { TrackData } from '../../data/track-data';
 import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from "../../services/spotify.service";
 
 @Component({
   selector: 'app-album-page',
@@ -15,11 +16,27 @@ export class AlbumPageComponent implements OnInit {
 	tracks:TrackData[];
 
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private spotifyService: SpotifyService
+  ) { }
 
   ngOnInit() {
+    // https://open.spotify.com/album/7xgzD7onYd1Pms93ZOtxxX
   	this.albumId = this.route.snapshot.paramMap.get('id');
   	//TODO: inject spotifyService and use it to get the album data and the tracks for the album
+    // this.spotifyService.getArtist('6sFIWsNpZYqfjUpaCgueju').then((artistData) => {
+    //   console.log(artistData)
+    // });
+    this.spotifyService.getAlbum(this.albumId)
+    .then((albumData) => {
+      this.album = albumData;
+    });
+
+    this.spotifyService.getTracksForAlbum(this.albumId)
+    .then((tracksData) => {
+      this.tracks = tracksData;
+    });
   }
 
 }
